acdfa8c94435b7d8c5c7f832ca280b9346969066
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-02 01:02:42 +0530

    removing html

--- Loan_Details/form.js
+++
@@ -1,8 +0,0 @@
-function calculateEMI() {
-    
-    const principal = parseFloat(document.getElementById('principal').value);
-    const rate = parseFloat(document.getElementById('rate').value) / 100 / 12;
-    const time = parseFloat(document.getElementById('time').value) * 12; 
-    const emi = (principal * rate * Math.pow(1 + rate, time)) / (Math.pow(1 + rate, time) - 1);
-    document.getElementById('result').innerHTML = `Monthly EMI: ${emi.toFixed(2)}`;
-}
