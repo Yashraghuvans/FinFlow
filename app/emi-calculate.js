3dd29f4702ccb17ae41d6a10f9641a4df3eea210
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-03 00:35:34 +0530

    Create emi-calculate.js

---
+++ app/emi-calculate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmiCalculate from '../Component/EmiCalculate';
+
+const EmiCalculatePage = () => {
+  return (
+    <div>
+      <EmiCalculate />
+    </div>
+  );
+};
+
+export default EmiCalculatePage;
