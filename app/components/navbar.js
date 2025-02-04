707e5eb65347a7491cde0f2e930d22e627652ccf
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-05 00:19:21 +0530

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,15 +1,22 @@
-import React from 'react'
+"use client"
+import Link from 'next/link';
+import React from 'react';
+
 
 const NavBar = () => {
-    return (
-        <div className='h-[145px] gap-[50px] w-full flex items-center px-[150px] justify-start fixed z-[99]'>
-            <img src="favicon.ico" alt="FinFlow Logo" className='h-[75px] rounded-[50px]' />
-            <h4 className='uppercase font-medium'>Home</h4>
-            <h4 className='uppercase font-medium'>About</h4>
-            <h4 className='uppercase font-medium'>Features</h4>
-            <h4 className='uppercase font-medium'>Login</h4>
-        </div>
-    )
-}
+  return (
+    <div className='h-[145px] gap-[50px] w-full flex items-center px-[150px] justify-start fixed z-[99]'>
+      <img src="favicon.ico" alt="FinFlow Logo" className='h-[75px] rounded-[50px]' />
+      <h4 className='uppercase font-medium'>Home</h4>
+      <h4 className='uppercase font-medium'>About</h4>
+      <h4 className='uppercase font-medium'>
+        <Link href='/loanDetail'>Features</Link>
+      </h4>
+      <h4 className='uppercase font-medium'>
+      <Link href='/signin'>SignUp</Link>
+      </h4>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
