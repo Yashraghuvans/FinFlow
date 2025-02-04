d77bb84c0899fdf9573c6eb43ca7ee1477a1a661
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-05 00:19:09 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,18 @@
 "use client"
 import React from 'react';
 import NavBar from './components/navbar';
-import Page1 from './components/page1';
-import Page2 from './components/page2';
+import Main from './components/main';
 
-const Main = () => {
+
+const Page = () => {
   return (
     <>
+   
     <NavBar/>
-    <Page1/>
-    <Page2/>
+    <Main/>
+    
     </>
   );
 };
 
-export default Main;
+export default Page;
