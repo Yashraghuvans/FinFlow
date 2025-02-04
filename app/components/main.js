fd9be5baa32ef2a0cf174b096e47eed640235e3b
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-05 00:19:33 +0530

    Create main.js

---
+++ app/components/main.js
@@ -0,0 +1,19 @@
+"use client"
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div id="main" className='relative bg-black/70 '>
+            <div className='h-screen w-full relative flex flex-col items-center justify-center text-center '>
+                <h1 className="relative text-[130px] font-extrabold backdrop-blur-sm ">
+                    Track. Plan. Prosper
+                </h1>
+                <h2 className='text-[35px] font-extrabold mt-[10px] mb-[20px] backdrop-blur-sm'>Welcome to FinFlow</h2>
+                <p className='text-[20px] font-medium w-[45%] backdrop-blur-sm'>FinFlow empowers homebuyers with a streamlined platform to track loan disbursements, payments, and interest, providing crucial insights and reminders to optimize their journey.</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Main
