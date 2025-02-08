cc5e74d49c0075f1f9c040e40bf842f679a7572b
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-09 00:48:19 +0530

    Create page.js

---
+++ app/signin/page.js
@@ -0,0 +1,59 @@
+//signin page
+"use client"
+import React from 'react'
+import { IoLogoGoogle } from "react-icons/io5";
+import { FaFacebook } from "react-icons/fa";
+import { TiVendorMicrosoft } from "react-icons/ti";
+import { FaXTwitter } from "react-icons/fa6";
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import NavBar from '../components/navbar';
+import Link from 'next/link';
+
+const SignIn = () => {
+
+  const notify = () => toast.success('Complete !', {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Bounce,
+  });
+  return (
+    <>
+      <NavBar />
+      <div className='w-full  h-full flex '>
+        <div className='h-full w-[50%] bg-gray-600  text-white text-[40px] font-semibold '>
+          <img src='favicon.ico' className='absolute top-[25%] left-[10%] h-96 w-96 rounded-lg mix-blend-multiply' />
+        </div>
+
+
+        <div className='h-full w-[50%] bg-gray-900 p-36 '>
+          <div className='flex flex-col gap-4 '>
+            <input type='text' placeholder='Name' className='p-3 text-white bg-gray-900 border-b-2 border-white' />
+            <input type='email' placeholder='Email' className='p-3  text-white bg-gray-900 border-b-2 border-white' />
+            <input type='password' placeholder='Password' className='p-3  text-white bg-gray-900 border-b-2 border-white' />
+            <Link href='/loanDetail'>
+              <button className='text-white bg-green-500 p-3 w-[100%] my-5 hover:bg-green-600 transition duration-600' onClick={notify}>Continue</button>
+            </Link>
+            <h3 className='text-white text-xl text-center'>OR</h3>
+            <hr className='border-b-2 border-white' />
+            <div className='flex justify-center gap-5 text-white text-3xl'>
+              <div><IoLogoGoogle /></div>
+              <div><FaFacebook /></div>
+              <div><TiVendorMicrosoft /></div>
+              <div><FaXTwitter /></div>
+            </div>
+          </div>
+        </div>
+        <ToastContainer />
+      </div>
+    </>
+  )
+}
+
+export default SignIn
