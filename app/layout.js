9c87f5023dadc0d6043087b6955a1d3a66fe5b42
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-09 00:48:28 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,27 @@
-"use clinet"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export const metadata = {
   title: "FinFlow",
-  description: "Created by YashRaghuvanshi",
+  description: "Created by Yash Raghuvanshi",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
