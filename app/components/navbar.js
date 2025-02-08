113b42a86e6fefea6daa742fe74cd98007d7a5ac
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-09 00:48:34 +0530

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -7,8 +7,12 @@ const NavBar = () => {
   return (
     <div className='h-[145px] gap-[50px] w-full flex items-center px-[150px] justify-start fixed z-[99]'>
       <img src="favicon.ico" alt="FinFlow Logo" className='h-[75px] rounded-[50px]' />
-      <h4 className='uppercase font-medium'>Home</h4>
-      <h4 className='uppercase font-medium'>About</h4>
+      <h4 className='uppercase font-medium'>
+        <Link href="/">Home</Link>
+        </h4>
+      <h4 className='uppercase font-medium'>
+        <Link href="/">About</Link>
+        </h4>
       <h4 className='uppercase font-medium'>
         <Link href='/loanDetail'>Features</Link>
       </h4>
