a42ae374023f85d81133b648947f1533c83ce305
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-10 15:27:28 +0530

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -2,24 +2,30 @@
 import Link from 'next/link';
 import React from 'react';
 
-
 const NavBar = () => {
   return (
-    <div className='h-[145px] gap-[50px] w-full flex items-center px-[150px] justify-start fixed z-[99]'>
-      <img src="favicon.ico" alt="FinFlow Logo" className='h-[75px] rounded-[50px]' />
-      <h4 className='uppercase font-medium'>
-        <Link href="/">Home</Link>
-        </h4>
-      <h4 className='uppercase font-medium'>
-        <Link href="/about">About</Link>
-        </h4>
-      <h4 className='uppercase font-medium'>
-        <Link href='/loanDetail'>Features</Link>
-      </h4>
-      <h4 className='uppercase font-medium'>
-      <Link href='/signin'>SignUp</Link>
-      </h4>
-    </div>
+    <nav className='fixed top-0 left-0 w-full z-[99] bg-black/80 backdrop-blur-sm'> 
+      <div className='container mx-auto px-4 md:px-8 lg:px-12 py-4 flex items-center justify-between'> 
+        <Link href="/" className="flex items-center"> 
+          <img src="favicon.ico" alt="FinFlow Logo" className='h-12 w-12 rounded-full' /> 
+          
+        </Link>
+        <div className='flex space-x-4 md:space-x-6 lg:space-x-8'>
+          <Link href="/" className='text-base md:text-lg font-medium uppercase hover:text-blue-500 transition'> 
+            Home
+          </Link>
+          <Link href="/about" className='text-base md:text-lg font-medium uppercase hover:text-blue-500 transition'>
+            About
+          </Link>
+          <Link href='/loanDetail' className='text-base md:text-lg font-medium uppercase hover:text-blue-500 transition'>
+            Features
+          </Link>
+          <Link href='/signin' className='text-base md:text-lg font-medium uppercase hover:text-blue-500 transition'>
+            SignUp
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 };
 
