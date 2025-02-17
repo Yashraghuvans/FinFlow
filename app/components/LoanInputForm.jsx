2b5c6a89f83baf3c033f979134e13ef668a962e9
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-17 11:20:45 +0530

    Update LoanInputForm.jsx

--- app/components/LoanInputForm.jsx
+++ app/components/LoanInputForm.jsx
@@ -9,6 +9,36 @@ function LoanInputForm({ onLoanSubmit, onCalculateEMI }) {
     const [downPayment, setDownPayment] = useState('');
     const [prePayment, setprePayment] = useState('');
     const bankNames = ['SBI', 'HDFC', 'Axis', 'ICICI', 'PNB', 'LIC', 'Citibank', 'Others'];
+    const locations = [
+        "Andhra Pradesh",
+        "Arunachal Pradesh",
+        "Assam",
+        "Bihar",
+        "Chhattisgarh",
+        "Goa",
+        "Gujarat",
+        "Haryana",
+        "Himachal Pradesh",
+        "Jharkhand",
+        "Karnataka",
+        "Kerala",
+        "Madhya Pradesh",
+        "Maharashtra",
+        "Manipur",
+        "Meghalaya",
+        "Mizoram",
+        "Nagaland",
+        "Odisha",
+        "Punjab",
+        "Rajasthan",
+        "Sikkim",
+        "Tamil Nadu",
+        "Telangana",
+        "Tripura",
+        "Uttar Pradesh",
+        "Uttarakhand",
+        "West Bengal"
+    ];
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -48,34 +78,39 @@ function LoanInputForm({ onLoanSubmit, onCalculateEMI }) {
         <div className="loan-input-form">
             <h2 className="text-xl font-semibold mb-4 text-gray-100">Enter Loan Details</h2>
             <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-                <label htmlFor="bankName" className="block text-gray-300 text-sm font-bold mb-2">Bank Name:</label>
-                <select
-                    id="bankName"
-                    value={bankName}
-                    onChange={(e) => setBankName(e.target.value)}
-                    required
-                    className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline bg-gray-700 text-white border-gray-600 focus:ring-blue-500 focus:border-blue-500"
-                >
-                    <option value="">Select Bank</option> 
-                    {bankNames.map((bank) => (
-                        <option key={bank} value={bank}>
-                            {bank}
-                        </option>
-                    ))}
-                </select>
-            </div>
+                <div className="mb-4">
+                    <label htmlFor="bankName" className="block text-gray-300 text-sm font-bold mb-2">Bank Name:</label>
+                    <select
+                        id="bankName"
+                        value={bankName}
+                        onChange={(e) => setBankName(e.target.value)}
+                        required
+                        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline bg-gray-700 text-white border-gray-600 focus:ring-blue-500 focus:border-blue-500"
+                    >
+                        <option value="">Select Bank</option>
+                        {bankNames.map((bank) => (
+                            <option key={bank} value={bank}>
+                                {bank}
+                            </option>
+                        ))}
+                    </select>
+                </div>
                 <div className="mb-4">
                     <label htmlFor="location" className="block text-gray-300 text-sm font-bold mb-2">Location:</label>
-                    <input
-                        type="text"
+                    <select
                         id="location"
                         value={location}
                         onChange={(e) => setLocation(e.target.value)}
                         required
-                        className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline bg-gray-700 text-white border-gray-600 focus:ring-blue-500 focus:border-blue-500"
-                        placeholder="Location"
-                    />
+                        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline bg-gray-700 text-white border-gray-600 focus:ring-blue-500 focus:border-blue-500"
+                    >
+                        <option value="">Select Location</option>
+                        {locations.map((loc) => (
+                            <option key={loc} value={loc}>
+                                {loc}
+                            </option>
+                        ))}
+                    </select>
                 </div>
                 <div className="mb-4">
                     <label htmlFor="timePeriod" className="block text-gray-300 text-sm font-bold mb-2">Time Period (Years):</label>
