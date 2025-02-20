35cf31be794566ac64771e04f98bddd5799eccca
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-20 12:15:45 +0530

    Create page.js

---
+++ app/contractorDashboard/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../components/navbar';
+
+const ContractorDashboard = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className='w-full min-h-screen bg-gray-800 flex justify-center items-center'>
+        <h1 className='text-white text-5xl font-semibold p-8'>Contractors dashboard is in progress</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ContractorDashboard;
