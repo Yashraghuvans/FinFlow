dda8f39767981c29f4e869c432ad89e954b00152
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-21 11:22:53 +0530

    Update page.js

--- app/contractorDashboard/page.js
+++ app/contractorDashboard/page.js
@@ -1,12 +1,20 @@
+"use client"
 import React from 'react';
 import NavBar from '../components/navbar';
+import PreEMIInputForm from '../components/PreEMIInputForm';
 
 const ContractorDashboard = () => {
+
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <NavBar />
-      <div className='w-full min-h-screen bg-gray-800 flex justify-center items-center'>
-        <h1 className='text-white text-5xl font-semibold p-8'>Contractors dashboard is in progress</h1>
+      <div className="w-full bg-gray-900 flex justify-center items-center py-10">
+        <h1 className="text-white text-5xl font-semibold text-center px-4">
+          Contractors dashboard
+        </h1>
+      </div>
+      <div className="w-full bg-gray-900 flex justify-center items-center py-10">
+        <PreEMIInputForm />
       </div>
     </div>
   );
