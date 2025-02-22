9ff116323aa81bc346ddd1706c86ea0be847caa0
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-02-22 14:33:45 +0530

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -8,7 +8,6 @@ const NavBar = () => {
       <div className='container mx-auto px-4 md:px-8 lg:px-12 py-4 flex items-center justify-between'> 
         <Link href="/" className="flex items-center"> 
           <img src="favicon.ico" alt="FinFlow Logo" className='h-12 w-12 rounded-full' /> 
-          
         </Link>
         <div className='flex space-x-4 md:space-x-6 lg:space-x-8'>
           <Link href="/" className='text-base md:text-lg font-medium uppercase hover:text-blue-500 transition'> 
