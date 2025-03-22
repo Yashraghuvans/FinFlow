784415b9908121830bbf280608233c73d7cfab04
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-22 16:29:46 +0530

    Update page.js

--- app/dashpage/page.js
+++ app/dashpage/page.js
@@ -1,38 +1,70 @@
 import Link from 'next/link';
 import React from 'react';
 import NavBar from '../components/navbar';
+import Image from 'next/image';
 const DashPage = () => {
     return (
         <div>
             <NavBar />
-            <div className='flex flex-col min-h-screen w-full bg-gray-800 items-center justify-center'> 
-                <div className='flex flex-col items-center justify-center py-16 px-4 w-full max-w-7xl'> 
-                    <h1 className='text-white font-semibold text-2xl mb-4'>Choose Who You Are</h1>
-                    <div className="flex flex-wrap justify-center gap-8 md:gap-16 lg:gap-24 w-full"> 
-                        <div className='bg-gray-700 border-2 p-6 rounded-lg w-full max-w-[300px] h-auto md:h-[400px] flex flex-col justify-center items-center gap-4 hover:bg-gray-800 transition-colors duration-400'>
-                            <h2 className='text-xl font-semibold'>Contractor</h2>
-                            <img
-                                src='https://png.pngtree.com/png-clipart/20190917/original/pngtree-construction-worker-cartoon-decorative-material-png-image_4601166.jpg'
-                                alt="Contractor image"
-                                className='max-h-[75%] w-auto'
-                            />
-                            <Link href="./contractorDashboard">
-                                <button className='p-3 px-7 bg-green-500 text-white text-xl cursor-pointer rounded-xl hover:bg-green-600 transition-colors duration-500'>Click</button>
-                            </Link>
+            <div className='flex h-screen  w-full bg-gray-800 items-center justify-center'>
+                <div className='flex gap-20  items-center justify-center py-16 p-4 w-full max-w-7xl pt-32'>
+                    <section>
+                        <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                            <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+                                <Image
+                                    height={64}
+                                    width={64}
+                                    src="/img2.png"
+                                    alt="Owner image"
+                                    className='h-[20vh] w-[20vw]'
+                                />
+                            </div>
+                            <div className="p-6">
+                                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                                    Contractor
+                                </h5>
+                                <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                                    Consist of all the analytics you need as a contractor.
+                                </p>
+                            </div>
+                            <div className="p-6 pt-0">
+                                <Link href="./contractorDashboard">
+                                    <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                                        Click
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
+                    </section>
 
-                        <div className='bg-gray-700 border-2 p-6 rounded-lg w-full max-w-[300px] h-auto md:h-[400px] flex flex-col justify-center items-center gap-4 hover:bg-gray-800 transition-colors duration-400'>
-                            <h2 className='text-xl font-semibold'>Owner</h2>
-                            <img
-                                src="https://i.pinimg.com/474x/50/77/2a/50772aefe8fef05f0b9a6e5c704049e0.jpg"
-                                alt="Owner image"
-                                className='max-h-[75%] w-auto'
-                            />
-                            <Link href="./ownerDashboard">
-                                <button className='p-3 px-7 bg-green-500 text-white text-xl cursor-pointer rounded-xl hover:bg-green-600 transition-colors duration-500'>Click</button>
-                            </Link>
+                    <section>
+                        <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                            <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+                                <Image
+                                    height={64}
+                                    width={64}
+                                    src="/img1.png"
+                                    alt="Owner image"
+                                    className='h-[20vh] w-[20vw]'
+                                />
+                            </div>
+                            <div className="p-6">
+                                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                                    Owner
+                                </h5>
+                                <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                                Consist of all the analytics you need as a owner.
+                                </p>
+                            </div>
+                            <div className="p-6 pt-0">
+                                <Link href="./ownerDashboard">
+                                    <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                                        Click
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
+                    </section>
                 </div>
             </div>
         </div>
