82daa12f8f6a0ad7d00d74ba8e4fd8d670e8284d
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-26 01:27:44 +0530

    Update page.js

--- app/contractorDashboard/page.js
+++ app/contractorDashboard/page.js
@@ -18,24 +18,26 @@ const ContractorDashboard = () => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-hidden bg-gray-900">
       <NavBar />
-      <div className="w-full bg-gray-900 flex justify-center items-center py-10">
+      <div className="w-full flex justify-center items-center py-10">
         <h1 className="text-white text-5xl font-semibold text-center pt-32 z-[99]">
           Contractors dashboard
         </h1>
       </div>
-      <div className="w-full bg-gray-900 flex flex-wrap justify-center items-center gap-6 p-4">
-        <div className="w-full lg:w-full flex justify-center items-center">
-          <PreEMIInputForm onSubmit={handleFormSubmit} />
+      <div className="w-full">
+        <div className="w-full flex justify-center items-center pl-2 pr-2">
+          <PreEMIInputForm onSubmit={handleFormSubmit} className="w-full m-0" />
         </div>
+      </div>
+      <div className="w-full flex flex-wrap justify-center items-center gap-6 p-4 mb-4">
         <div className="w-full lg:w-1/2 border-2 border-white p-5">
           <h1 className="text-3xl text-center p-2 pb-6">Budget Analysis Chart</h1>
           <hr className="border-b-2 bg-white" />
           <BudgetPieChart projects={formData} />
         </div>
       </div>
-      <div className="w-full bg-gray-900 p-8 border-2 border-white">
+      <div className="w-full p-8 border-2 border-white">
         <h2 className="text-2xl font-semibold mb-4 text-gray-100">Details</h2>
         <div className="w-full lg:w-1/2">
           <FormInputDetails formData={formData} />
